807040aa221592626018435ef1ecd8e98af48bda
Han <inte098@naver.com>
2024-09-06 14:23:47 +0900

    섹션4 - MyBatis 적용(기본) ItemMapper 인터페이스 생성 itemMapper.xml 작성(save, update, find)

---
+++ src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -0,0 +1,24 @@
+package hello.itemservice.repository.mybatis;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
+public interface ItemMapper {
+
+    void save(Item item);
+
+    // 파라미터가 2개 이상이라면 @Param을 넣어야 한다.
+    void update(@Param("id") Long id, @Param("updateParam") ItemUpdateDto updateParam);
+
+    Optional<Item> findById(Long id);
+
+    List<Item> findAll(ItemSearchCond itemSearchCond);
+    
+}
